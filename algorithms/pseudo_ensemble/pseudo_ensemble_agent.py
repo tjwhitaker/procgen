3326f8df644e0e5709b0c639b9c27244a89e4a00
Tim Whitaker <camp4climber@gmail.com>
2020-06-26 10:57:09 -0600

    Run 4 model ensemble

--- algorithms/pseudo_ensemble/pseudo_ensemble_agent.py
+++ algorithms/pseudo_ensemble/pseudo_ensemble_agent.py
@@ -1,35 +1,17 @@
 import numpy as np
-import copy
+from copy import deepcopy
 from random import random
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
 
 import ray
 
-from multiprocessing import Pool
-
-
-def predict(data):
-    print(data)
-    return 1
-
 
 class PseudoEnsembleAgent(PPOTrainer):
     def __init__(self, config=None, env=None, logger_creator=None):
         super().__init__(config, env, logger_creator)
-
+        self.og_weights = []
         self.ensemble_weights = []
-        self.og_weights = self.get_policy().get_weights()
-
-        for i in range(4):
-            new_weights = self.prune_weights(self.og_weights, 0.1)
-            self.ensemble_weights.append(new_weights)
-
-        # self.ensemble = []
-
-        # for i in range(4):
-        #     self.ensemble.append(
-        #         self.get_policy().model.base_model.get_config())
 
     def compute_action(self,
                        observation,
@@ -40,20 +22,47 @@ class PseudoEnsembleAgent(PPOTrainer):
                        policy_id=DEFAULT_POLICY_ID,
                        full_fetch=False,
                        explore=None):
+        if state is None:
+            state = []
+        preprocessed = self.workers.local_worker().preprocessors[
+            policy_id].transform(observation)
+        filtered_obs = self.workers.local_worker().filters[policy_id](
+            preprocessed, update=False)
+
+        # Figure out the current (sample) time step and pass it into Policy.
+        self.global_vars["timestep"] += 1
+
         ensemble_actions = []
 
         for weights in self.ensemble_weights:
             self.get_policy().set_weights(weights)
-            ensemble_actions.append(super().compute_action(observation, state, prev_action,
-                                                           prev_reward, info, policy_id, full_fetch, explore))
+            result = self.get_policy(policy_id).compute_single_action(
+                filtered_obs,
+                state,
+                prev_action,
+                prev_reward,
+                info,
+                clip_actions=self.config["clip_actions"],
+                explore=explore,
+                timestep=self.global_vars["timestep"])
+            ensemble_actions.append(result[0])
 
-        return 1
-        # return max(set(ensemble_actions), key=ensemble_actions.count)
+        return max(set(ensemble_actions), key=ensemble_actions.count)
+
+    def restore(self, checkpoint_path):
+        super().restore(checkpoint_path)
+        self.ensemble_weights = []
+        self.og_weights = self.get_policy().get_weights()
+
+        for i in range(4):
+            new_weights = self.prune_weights(self.og_weights, 0.1)
+            self.ensemble_weights.append(new_weights)
 
     def prune_weights(self, weights, probability):
-        w = copy.deepcopy(weights)
+        w = deepcopy(weights)
         for layer in w.keys():
-            for weight in np.nditer(w[layer], op_flags=['readwrite']):
-                if random() < probability:
-                    weight[...] = 0
+            if ("pi" not in layer) and ("vf" not in layer):
+                for weight in np.nditer(w[layer], op_flags=['readwrite']):
+                    if random() < probability:
+                        weight[...] = 0
         return w
--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -53,7 +53,7 @@ class ImpalaCNN(TFModelV2):
         x = tf.keras.layers.Flatten()(x)
         x = tf.keras.layers.ReLU()(x)
         x = tf.keras.layers.Dense(
-            units=256, activation="tanh", name="hidden")(x)
+            units=256, activation="relu", name="hidden")(x)
         logits = tf.keras.layers.Dense(units=num_outputs, name="pi")(x)
         value = tf.keras.layers.Dense(units=1, name="vf")(x)
         self.base_model = tf.keras.Model(inputs, [logits, value])
@@ -70,4 +70,4 @@ class ImpalaCNN(TFModelV2):
 
 
 # Register model in ModelCatalog
-ModelCatalog.register_custom_model("impala_cnn_tf", ImpalaCNN)
+ModelCatalog.register_custom_model("impala_cnn", ImpalaCNN)
