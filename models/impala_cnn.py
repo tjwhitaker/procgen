67630dbb5adf8e9f249de40c449f1ce06fc39486
Tim Whitaker <camp4climber@gmail.com>
2020-07-04 10:58:09 -0600

    Remove action mask

--- models/impala_cnn.py
+++ models/impala_cnn.py
@@ -68,12 +68,7 @@ class ImpalaCNN(TFModelV2):
         obs = tf.cast(input_dict["obs"], tf.float32)
         logits, self._value = self.base_model(obs)
 
-        action_mask = np.array(
-            [1., 1., 1., 1., 1., 1., 1., 1., 1., 0., 0., 0., 0., 0., 0.], dtype=np.float32)
-
-        inf_mask = tf.maximum(tf.math.log(action_mask), tf.float16.min)
-
-        return logits + inf_mask, state
+        return logits, state
 
     def value_function(self):
         return tf.reshape(self._value, [-1])
