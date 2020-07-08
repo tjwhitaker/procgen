1c03e26b5ddac5c5e35e3fcc0cbd9804b03096f0
Tim Whitaker <camp4climber@gmail.com>
2020-07-08 08:47:21 -0600

    Work on pe

--- algorithms/pseudo_ensemble/pseudo_ensemble_agent.py
+++ algorithms/pseudo_ensemble/pseudo_ensemble_agent.py
@@ -8,79 +8,104 @@ import ray
 
 
 def prune_weights(weights, probability):
-    w = deepcopy(weights)
-
     for layer in weights.keys():
-        if ("pi" not in layer) and ("vf" not in layer):
-            for weight in np.nditer(w[layer], op_flags=['readwrite']):
+        if "hidden/kernel" in layer:
+            for w in np.nditer(weights[layer], op_flags=['readwrite']):
                 if random() < probability:
-                    weight[...] = 0
+                    w[...] = 0
 
     return weights
 
 
 def add_gaussian_noise(weights, scale):
-    w = deepcopy(weights)
-
     for layer in weights.keys():
-        if ("pi" not in layer) and ("vf" not in layer):
-            for weight in np.nditer(w[layer], op_flags=['readwrite']):
-                weight[...] += np.random.normal(0, scale)
+        if "hidden/kernel" in layer:
+            for w in np.nditer(weights[layer], op_flags=['readwrite']):
+                w[...] += np.random.normal(0, scale)
 
     return weights
+
+
+def calculate_confidence(values):
+    x = values - max(values)
+    softmax = np.exp(x) / np.sum(np.exp(x))
+    return max(softmax)
 
 
 class PseudoEnsembleAgent(PPOTrainer):
     def __init__(self, config=None, env=None, logger_creator=None):
         super().__init__(config, env, logger_creator)
         self.ensemble_weights = []
+        self.original_weights = []
 
-    # def compute_action(self,
-    #                    observation,
-    #                    state=None,
-    #                    prev_action=None,
-    #                    prev_reward=None,
-    #                    info=None,
-    #                    policy_id=DEFAULT_POLICY_ID,
-    #                    full_fetch=False,
-    #                    explore=None):
-    #     if state is None:
-    #         state = []
-    #     preprocessed = self.workers.local_worker().preprocessors[
-    #         policy_id].transform(observation)
-    #     filtered_obs = self.workers.local_worker().filters[policy_id](
-    #         preprocessed, update=False)
+    def compute_action(self,
+                       observation,
+                       state=None,
+                       prev_action=None,
+                       prev_reward=None,
+                       info=None,
+                       policy_id=DEFAULT_POLICY_ID,
+                       full_fetch=False,
+                       explore=None):
+        if state is None:
+            state = []
+        preprocessed = self.workers.local_worker().preprocessors[
+            policy_id].transform(observation)
+        filtered_obs = self.workers.local_worker().filters[policy_id](
+            preprocessed, update=False)
 
-    #     # Figure out the current (sample) time step and pass it into Policy.
-    #     self.global_vars["timestep"] += 1
+        self.global_vars["timestep"] += 1
 
-    #     ensemble_actions = []
+        # Run through full network
+        action, _, info = self.get_policy(policy_id).compute_single_action(
+            filtered_obs,
+            state,
+            prev_action,
+            prev_reward,
+            info,
+            clip_actions=self.config["clip_actions"],
+            explore=explore,
+            timestep=self.global_vars["timestep"])
 
-    #     for weights in self.ensemble_weights:
-    #         self.get_policy().set_weights(weights)
-    #         result = self.get_policy(policy_id).compute_single_action(
-    #             filtered_obs,
-    #             state,
-    #             prev_action,
-    #             prev_reward,
-    #             info,
-    #             clip_actions=self.config["clip_actions"],
-    #             explore=explore,
-    #             timestep=self.global_vars["timestep"])
-    #         ensemble_actions.append(result[0])
+        # If confidence is low, run through ensemble
+        if calculate_confidence(info['action_dist_inputs']) < 0.75:
+            ensemble_actions = []
 
-    #     return max(set(ensemble_actions), key=ensemble_actions.count)
+            for weights in self.ensemble_weights:
+                self.get_policy().set_weights(weights)
+                result = self.get_policy(policy_id).compute_single_action(
+                    filtered_obs,
+                    state,
+                    prev_action,
+                    prev_reward,
+                    info,
+                    clip_actions=self.config["clip_actions"],
+                    explore=explore,
+                    timestep=self.global_vars["timestep"])
+                ensemble_actions.append(result[0])
+
+            self.get_policy().set_weights(self.original_weights)
+
+            print(ensemble_actions)
+
+            return max(set(ensemble_actions), key=ensemble_actions.count)
+        else:
+            return action
 
     def restore(self, checkpoint_path):
         super().restore(checkpoint_path)
+        self.original_weights = self.get_policy().get_weights()
         self.create_ensemble()
 
     def create_ensemble(self):
         self.ensemble_weights = []
-        weights = self.get_policy().get_weights()
 
         for i in range(8):
-            # new_weights = prune_weights(weights, 0.05)
-            new_weights = add_gaussian_noise(weights, 1.0)
+            w = deepcopy(self.original_weights)
+            new_weights = prune_weights(w, 0.5)
+            # new_weights = add_gaussian_noise(deepcopy(self.original_weights), 1.0)
 
             self.ensemble_weights.append(new_weights)
+
+        # # TRUE
+        # print(self.ensemble_weights[0] == self.ensemble_weights[1])
