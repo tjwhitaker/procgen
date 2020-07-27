c4c54b0b5f2e56794dbfc6430e29da0b08e6c63d
Tim Whitaker <camp4climber@gmail.com>
2020-07-27 16:13:26 -0600

    Submit ppo for round 1

--- algorithms/pseudo_ensemble/pseudo_ensemble_agent.py
+++ algorithms/pseudo_ensemble/pseudo_ensemble_agent.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 from random import random
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
+from ray.rllib.utils import merge_dicts
 import ray
 
 
@@ -42,8 +43,27 @@ def calculate_confidence(values):
 class PseudoEnsembleAgent(PPOTrainer):
     def __init__(self, config=None, env=None, logger_creator=None):
         super().__init__(config, env, logger_creator)
-        self.ensemble_weights = []
+        self.ensemble = []
         self.original_weights = []
+
+        # Config used to evaluate ensemble models for selection
+        extra_config = deepcopy(self.config["evaluation_config"])
+        extra_config.update({
+            "batch_mode": "complete_episodes",
+            "rollout_fragment_length": 1,
+            "in_evaluation": True,
+        })
+
+        self.config["evaluation_num_episodes"] = 1
+        self.config["evaluation_num_workers"] = 0
+        self.config["env_config"]["use_sequential_levels"] = True
+        self.config["env_config"]["distribution_mode"] = "easy"
+
+        self.evaluation_workers = self._make_workers(
+            self.env_creator,
+            self._policy,
+            merge_dicts(self.config, extra_config),
+            num_workers=0)
 
     def compute_action(self,
                        observation,
@@ -75,10 +95,10 @@ class PseudoEnsembleAgent(PPOTrainer):
             timestep=self.global_vars["timestep"])
 
         # If confidence is low, run through ensemble
-        if calculate_confidence(info['action_dist_inputs']) < 0.65:
+        if calculate_confidence(info['action_dist_inputs']) < 0.50:
             ensemble_actions = []
 
-            for weights in self.ensemble_weights:
+            for weights in self.ensemble:
                 self.get_policy().set_weights(weights)
                 result = self.get_policy(policy_id).compute_single_action(
                     filtered_obs,
@@ -103,25 +123,39 @@ class PseudoEnsembleAgent(PPOTrainer):
         self.create_ensemble()
 
     def create_ensemble(self):
-        self.ensemble_weights = []
-
-        for i in range(8):
-            w = deepcopy(self.original_weights)
-            new_weights = prune_weights(w, 0.05)
-            # new_weights = add_gaussian_noise(w, 0.1)
-            self.ensemble_weights.append(new_weights)
+        # for i in range(8):
+        #     w = deepcopy(self.original_weights)
+        #     new_weights = prune_weights(w, 0.05)
+        #     # new_weights = add_gaussian_noise(w, 0.1)
+        #     self.ensemble_weights.append(new_weights)
 
         # NK PE
-        # population_size = 8
-        # fitness_scores = []
+        population_size = 16
 
-        # self.generate_population(population_size)
-        # self.evaluate_fitness()
+        population = self.generate_population(population_size)
+        fitness = self.evaluate_fitness(population)
 
-        # # Evaluate Fitness
-        # for i in range(population_size):
-        #     self.get_policy().set_weights(self.ensemble_weights[i])
-        #     metrics = self._evaluate()
-        #     fitness_scores.append(metrics['evaluation']['episode_reward_mean'])
+        # Run NK Selection
+        # Just select top 8 for now?
+        self.ensemble = np.delete(population, np.argsort(fitness)[:8])
 
-        # # Run NK Selection
+    def generate_population(self, population_size):
+        population = []
+
+        for _ in range(population_size):
+            w = deepcopy(self.original_weights)
+            population.append(prune_weights(w, 0.05))
+
+        return population
+
+    def evaluate_fitness(self, population):
+        scores = []
+
+        for model in population:
+            self.get_policy().set_weights(model)
+            metrics = self._evaluate()
+            scores.append(metrics['evaluation']['episode_reward_mean'])
+
+        print(scores)
+
+        return scores
