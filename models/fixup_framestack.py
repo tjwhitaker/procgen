0923d664c1e6708fc706cc456b4b4dc041bcd4c9
Tim Whitaker <camp4climber@gmail.com>
2020-07-29 08:48:44 -0600

    Remove extra residual layers and remove reward clipping

--- models/fixup_framestack.py
+++ models/fixup_framestack.py
@@ -34,10 +34,10 @@ class FixupFS(TorchModelV2, nn.Module):
                 FixupResidual(depth_out, 8),
             ])
             depth_in = depth_out
-        layers.extend([
-            FixupResidual(depth_in, 8),
-            FixupResidual(depth_in, 8),
-        ])
+        # layers.extend([
+        #     FixupResidual(depth_in, 8),
+        #     FixupResidual(depth_in, 8),
+        # ])
         self.conv_layers = nn.Sequential(*layers)
         self.hidden_fc = nn.Linear(
             in_features=4096, out_features=256)
