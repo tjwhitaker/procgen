9497b861706075ba7936b4e9eeaf651a20685596
Tim Whitaker <camp4climber@gmail.com>
2020-08-05 15:34:30 -0600

    Use bigger model and don't framestack

--- models/fixup.py
+++ models/fixup.py
@@ -44,17 +44,15 @@ class FixupCNN(TorchModelV2, nn.Module):
         self.logits_fc = nn.Linear(in_features=256, out_features=num_outputs)
         self.value_fc = nn.Linear(in_features=256, out_features=1)
 
-        print(sum(p.numel() for p in self.parameters()))
-
     def forward(self, input_dict, state, seq_lens):
         x = input_dict["obs"].float()
         x = x / 255.0  # scale to 0-1
         x = x.permute(0, 3, 1, 2).contiguous()
         x = self.conv_layers(x)
-        x = nn.functional.leaky_relu(x)
+        x = nn.functional.relu(x)
         x = x.view(x.shape[0], -1)
         x = self.hidden_fc(x)
-        x = nn.functional.leaky_relu(x)
+        x = nn.functional.relu(x)
 
         logits = self.logits_fc(x)
         value = self.value_fc(x)
@@ -83,11 +81,11 @@ class FixupResidual(nn.Module):
         self.scale = nn.Parameter(torch.ones([depth, 1, 1]))
 
     def forward(self, x):
-        x = nn.functional.leaky_relu(x)
+        x = nn.functional.relu(x)
         out = x + self.bias1
         out = self.conv1(out)
         out = out + self.bias2
-        out = nn.functional.leaky_relu(out)
+        out = nn.functional.relu(out)
         out = out + self.bias3
         out = self.conv2(out)
         out = out * self.scale
