c66c881432751d5ab3d884d01eee1f819672e00c
Tim Whitaker <camp4climber@gmail.com>
2020-08-08 10:02:34 -0600

    Add pseudo-sequential-levels

--- envs/time_limit_fs.py
+++ envs/time_limit_fs.py
@@ -1,4 +1,5 @@
 import gym
+import inspect
 import numpy as np
 from envs.procgen_env_wrapper import ProcgenEnvWrapper
 from collections import deque
@@ -6,34 +7,32 @@ from ray.tune import registry
 
 
 class TimeLimit(gym.Wrapper):
-    """
-    Grant a curiosity bonus based on a perceptual hash.
-    """
-
     def __init__(self, env):
         super(TimeLimit, self).__init__(env)
 
         self.env = env
-        self.episode_reward = 0
         self.episode_step = 0
 
     def reset(self, **kwargs):
-        self.episode_reward = 0
         self.episode_step = 0
 
         return self.env.reset(**kwargs)
 
     def step(self, action):
-        if self.env.env_name == 'coinrun' and self.episode_step > 300:
-            state, reward, done, info = self.env.step(-1)
-        elif self.env.env_name == 'miner' and self.episode_step > 300:
-            state, reward, done, info = self.env.step(-1)
-        elif self.env.env_name == 'bigfish' and self.episode_step > 850:
-            state, reward, done, info = self.env.step(-1)
-        else:
-            state, reward, done, info = self.env.step(action)
+        # if self.env.env_name == 'coinrun' and self.episode_step > 300:
+        #     state, reward, done, info = self.env.step(-1)
+        # elif self.env.env_name == 'miner' and self.episode_step > 300:
+        #     state, reward, done, info = self.env.step(-1)
+        # elif self.env.env_name == 'bigfish' and self.episode_step > 850:
+        #     state, reward, done, info = self.env.step(-1)
+        # else:
+        state, reward, done, info = self.env.step(action)
 
-        self.episode_reward += reward
+        if hasattr(self.env, 'rollout') and not self.env.rollout:
+            if done and reward > 0:
+                self.env.step(-1)
+                done = False
+
         self.episode_step += 1
 
         return state, reward, done, info
