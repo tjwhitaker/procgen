2f13e1f179d294e8cbe59726b9d5c7f4859b9bb5
Tim Whitaker <camp4climber@gmail.com>
2020-08-20 15:00:07 -0700

    Remove environment specific logic

--- envs/custom_wrappers.py
+++ envs/custom_wrappers.py
@@ -7,61 +7,6 @@ from collections import deque
 from ray.tune import registry
 
 
-class ReduceActions(gym.Wrapper):
-    def __init__(self, env):
-        super(ReduceActions, self).__init__(env)
-        self.action_map = [*range(15)]
-
-        # Coinrun
-        # L, LU, U, RU, R, ()
-        # 1, 2, 4, 5, 7, 8
-        if self.env.env_name == "coinrun":
-            self.action_map = [1, 2, 4, 5, 7, 8]
-            self.action_space = gym.spaces.Discrete(6)
-
-        # Bigfish
-        # L, LU, U, RU, R, RD, D, LD
-        # 0, 1, 2, 3, 5, 6, 7, 8, ()
-        if self.env.env_name == "bigfish":
-            self.action_map = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-            self.action_space = gym.spaces.Discrete(9)
-
-        # Miner
-        # L, D, R, U
-        # 1, 3, 5, 7
-        if self.env.env_name == "miner":
-            self.action_map = [1, 3, 5, 7]
-            self.action_space = gym.spaces.Discrete(4)
-
-    def step(self, action):
-        return self.env.step(self.action_map[action])
-
-
-class TimeLimit(gym.Wrapper):
-    def __init__(self, env, rollout):
-        super(TimeLimit, self).__init__(env)
-        self.episode_step = 0
-        self.rollout = rollout
-
-    def reset(self):
-        self.episode_step = 0
-        return self.env.reset()
-
-    def step(self, action):
-        self.episode_step += 1
-
-        if self.env.env_name == 'coinrun' and self.episode_step > 500:
-            state, reward, done, info = self.env.step(-1)
-        elif self.env.env_name == 'miner' and self.episode_step > 500:
-            state, reward, done, info = self.env.step(-1)
-        elif self.env.env_name == 'bigfish' and self.episode_step > 1000:
-            state, reward, done, info = self.env.step(-1)
-        else:
-            state, reward, done, info = self.env.step(action)
-
-        return state, reward, done, info
-
-
 class ContinuousLife(gym.Wrapper):
     def __init__(self, env, rollout):
         super(ContinuousLife, self).__init__(env)
@@ -114,14 +59,11 @@ class FrameSkip(gym.Wrapper):
     def step(self, action):
         total_reward = 0
 
-        if self.env.env_name == "coinrun" or self.env.env_name == "bigfish":
-            for _ in range(self.n):
-                state, reward, done, info = self.env.step(action)
-                total_reward += reward
-                if done:
-                    break
-        else:
-            state, total_reward, done, info = self.env.step(action)
+        for _ in range(self.n):
+            state, reward, done, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
 
         return state, total_reward, done, info
 
@@ -130,8 +72,6 @@ def create_env(config):
     config = copy(config)
     rollout = config.pop("rollout")
     env = ProcgenEnvWrapper(config)
-    env = ReduceActions(env)
-    env = TimeLimit(env, rollout)
     # env = ContinuousLife(env, rollout)
     env = FrameStack(env, 3)
     env = FrameSkip(env, 1)
