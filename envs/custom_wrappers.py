428809395ace88c321ec365bc8167ea52fbed7ef
Tim Whitaker <camp4climber@gmail.com>
2020-08-26 14:01:57 -0700

    Raise LR

--- envs/custom_wrappers.py
+++ envs/custom_wrappers.py
@@ -97,51 +97,51 @@ class ReduceActions(gym.Wrapper):
         self.env.step(-1)
 
 
-class ContinuousLife(gym.Wrapper):
-    def __init__(self, env, rollout):
-        super(ContinuousLife, self).__init__(env)
-        self.rollout = rollout
-        self.episode_reward = 0
+# class ContinuousLife(gym.Wrapper):
+#     def __init__(self, env, rollout):
+#         super(ContinuousLife, self).__init__(env)
+#         self.rollout = rollout
+#         self.episode_reward = 0
 
-        # See https://discourse.aicrowd.com/t/getting-rmax-from-environment/3362
-        self.reward_max = {
-            'coinrun': 10,
-            'starpilot': 64,
-            'caveflyer': 12,
-            'dodgeball': 19,
-            'fruitbot': 32.4,
-            'chaser': 13,
-            'miner': 13,
-            'jumper': 10,
-            'leaper': 10,
-            'maze': 10,
-            'bigfish': 40,
-            'heist': 10,
-            'climber': 12.6,
-            'plunder': 30,
-            'ninja': 10,
-            'bossfight': 13,
-            'caterpillar': 24,
-        }
+#         # See https://discourse.aicrowd.com/t/getting-rmax-from-environment/3362
+#         self.reward_max = {
+#             'coinrun': 10,
+#             'starpilot': 64,
+#             'caveflyer': 12,
+#             'dodgeball': 19,
+#             'fruitbot': 32.4,
+#             'chaser': 13,
+#             'miner': 13,
+#             'jumper': 10,
+#             'leaper': 10,
+#             'maze': 10,
+#             'bigfish': 40,
+#             'heist': 10,
+#             'climber': 12.6,
+#             'plunder': 30,
+#             'ninja': 10,
+#             'bossfight': 13,
+#             'caterpillar': 24,
+#         }
 
-    def reset(self):
-        self.episode_reward = 0
-        return self.env.reset()
+#     def reset(self):
+#         self.episode_reward = 0
+#         return self.env.reset()
 
-    def step(self, action):
-        state, reward, done, info = self.env.step(action)
+#     def step(self, action):
+#         state, reward, done, info = self.env.step(action)
 
-        self.episode_reward += reward
+#         self.episode_reward += reward
 
-        if not self.rollout:
-            # Need to know max reward to know if we've completed level
-            # Previous solution used done && current step reward > 0
-            # Errors when you get a reward and die in the same frame (bigfish)
-            if done and (self.episode_reward >= self.reward_max[self.env.env_name]):
-                self.reset()
-                done = False
+#         if not self.rollout:
+#             # Need to know max reward to know if we've completed level
+#             # Previous solution used done && current step reward > 0
+#             # Errors when you get a reward and die in the same frame (bigfish)
+#             if done and (self.episode_reward >= self.reward_max[self.env.env_name]):
+#                 self.reset()
+#                 done = False
 
-        return state, reward, done, info
+#         return state, reward, done, info
 
 
 class FrameSkip(gym.Wrapper):
