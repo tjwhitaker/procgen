10eea1c302b14bc6a782a724b1b62856f2877761
Tim Whitaker <camp4climber@gmail.com>
2020-08-27 14:32:59 -0700

    Use large network with lag frames

--- envs/custom_wrappers.py
+++ envs/custom_wrappers.py
@@ -96,6 +96,54 @@ class ReduceActions(gym.Wrapper):
         self.env.step(-1)
 
 
+class ContinuousLife(gym.Wrapper):
+    def __init__(self, env, rollout):
+        super(ContinuousLife, self).__init__(env)
+        self.rollout = rollout
+        self.episode_reward = 0
+
+        # See https://discourse.aicrowd.com/t/getting-rmax-from-environment/3362
+        self.reward_max = {
+            'coinrun': 10,
+            'starpilot': 64,
+            'caveflyer': 12,
+            'dodgeball': 19,
+            'fruitbot': 32.4,
+            'chaser': 13,
+            'miner': 13,
+            'jumper': 10,
+            'leaper': 10,
+            'maze': 10,
+            'bigfish': 40,
+            'heist': 10,
+            'climber': 12.6,
+            'plunder': 30,
+            'ninja': 10,
+            'bossfight': 13,
+            'caterpillar': 24,
+        }
+
+    def reset(self):
+        self.episode_reward = 0
+        return self.env.reset()
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+
+        self.episode_reward += reward
+
+        if not self.rollout:
+            # Need to know max reward to know if we've completed level
+            # Previous solution used done && current step reward > 0
+            # Errors when you get a reward and die in the same frame (bigfish)
+            if done and (self.episode_reward >= self.reward_max[self.env.env_name]):
+                print(self.episode_reward)
+                self.reset()
+                done = False
+
+        return state, reward, done, info
+
+
 class FrameStack(gym.Wrapper):
     def __init__(self, env, n):
         super(FrameStack, self).__init__(env)
@@ -120,6 +168,7 @@ class FrameStack(gym.Wrapper):
         return self._get_ob(), reward, done, info
 
     def _get_ob(self):
+        # Stack last n frames skip 1
         frames = [self.buffer[-1], self.buffer[-3], self.buffer[-5]]
         return np.concatenate(frames, axis=2)
 
@@ -129,6 +178,7 @@ def create_env(config):
     rollout = config.pop("rollout")
     env = ProcgenEnvWrapper(config)
     env = ReduceActions(env)
+    # env = ContinuousLife(env, rollout)
     env = FrameStack(env, 6)
     return env
 
