5bbc39dde4ac9df329c419260c7a7e97b33566a6
Tim Whitaker <camp4climber@gmail.com>
2020-09-03 20:16:20 -0700

    Retry failures

--- envs/custom_wrappers.py
+++ envs/custom_wrappers.py
@@ -144,31 +144,6 @@ class ContinuousLife(gym.Wrapper):
         return state, reward, done, info
 
 
-class ImproveContrast(gym.Wrapper):
-    def __init__(self, env):
-        super(ImproveContrast, self).__init__(env)
-
-    def reset(self):
-        state = self.env.reset()
-        return self.process_image(state)
-
-    def step(self, action):
-        state, reward, done, info = self.env.step(action)
-        return self.process_image(state), reward, done, info
-
-    def process_image(self, state):
-        for i in range(state.shape[-1]):
-            ch = state[:, :, i]
-            minval = np.percentile(ch, 2)
-            maxval = np.percentile(ch, 98)
-            pixels = np.clip(ch, minval, maxval)
-            pixels = ((pixels - minval) / (maxval - minval)) * 255.0
-
-            state[:, :, i] = pixels
-
-        return state
-
-
 class FrameStack(gym.Wrapper):
     def __init__(self, env, n):
         super(FrameStack, self).__init__(env)
@@ -226,11 +201,69 @@ class SubFrameStack(gym.Wrapper):
         return np.concatenate(frames, axis=2)
 
 
+class RetryFailures(gym.Wrapper):
+    def __init__(self, env, rollout):
+        super(RetryFailures, self).__init__(env)
+        self.rollout = rollout
+        self.episode_reward = 0
+        self.completed = True
+        self.reward_max = {
+            'coinrun': 10,
+            'starpilot': 64,
+            'caveflyer': 12,
+            'dodgeball': 19,
+            'fruitbot': 32.4,
+            'chaser': 13,
+            'miner': 13,
+            'jumper': 10,
+            'leaper': 10,
+            'maze': 10,
+            'bigfish': 40,
+            'heist': 10,
+            'climber': 12.6,
+            'plunder': 30,
+            'ninja': 10,
+            'bossfight': 13,
+            'caterpillar': 24,
+        }
+
+    def reset(self):
+        obs = self.env.reset()
+
+        if not self.rollout:
+            if self.completed:
+                self.checkpoint_state = self.unwrapped.env.env.callmethod(
+                    "get_state")
+            else:
+                self.unwrapped.env.env.callmethod(
+                    "set_state", self.checkpoint_state)
+                # Take default step to get correct obs
+                obs, _, _, _ = self.env.step(4)
+
+        return obs
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+
+        self.episode_reward += reward
+
+        if not self.rollout and done:
+            if self.episode_reward >= self.reward_max[self.env.env_name]:
+                self.completed = True
+            else:
+                self.completed = False
+
+            self.episode_reward = 0
+
+        return state, reward, done, info
+
+
 def create_env(config):
     config = copy(config)
     rollout = config.pop("rollout")
     env = ProcgenEnvWrapper(config)
     env = ReduceActions(env)
+    env = RetryFailures(env, rollout)
     env = SubFrameStack(env, 2)
     return env
 
