8d3cddad03ed2bdb64f600f7fc0de91169df4b1f
Tim Whitaker <camp4climber@gmail.com>
2020-10-06 19:58:07 -0700

    Log scale, small ensemble, frame stack

--- envs/custom_wrappers.py
+++ envs/custom_wrappers.py
@@ -246,10 +246,8 @@ class ShapeReward(gym.Wrapper):
         state, reward, done, info = self.env.step(action)
 
         if not self.rollout:
-            if reward > 1:
-                reward = np.log(reward+1)
-            if reward > 0 and reward < 1:
-                reward = 1.0
+            if reward > 0:
+                reward = np.log(reward+2)
 
         return state, reward, done, info
 
@@ -260,9 +258,9 @@ def create_env(config):
 
     env = ProcgenEnvWrapper(config)
     env = ReduceActions(env)
-    env = DiffStack(env, 2)
+    # env = DiffStack(env, 2)
     # env = ContinuousLife(env, rollout, return_max)
-    # env = FrameStack(env, 3)
+    env = FrameStack(env, 3)
     env = ShapeReward(env, rollout)
 
     return env
