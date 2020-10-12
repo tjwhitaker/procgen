917f0ade2295986e5a6f16d82fae0e4b9485a2b3
Tim Whitaker <camp4climber@gmail.com>
2020-10-12 07:51:35 -0700

    High LR Stepped Entropy

--- envs/custom_wrappers.py
+++ envs/custom_wrappers.py
@@ -61,7 +61,7 @@ class ReduceActions(gym.Wrapper):
 
             self.unwrapped.env.env.callmethod("set_state", base_state)
 
-        movement_states = {}
+        # movement_states = {}
 
         # ###############
         # # Test Movement
@@ -218,33 +218,9 @@ class DiffStack(gym.Wrapper):
         frames = [self.frames[1], abs(self.frames[1] - self.frames[0])]
         return np.concatenate(frames, axis=2)
 
-
-class ContinuousLife(gym.Wrapper):
-    def __init__(self, env, rollout, reward_max):
-        super(ContinuousLife, self).__init__(env)
-        self.rollout = rollout
-        self.episode_reward = 0
-        self.episode_step = 0
-        self.reward_max = reward_max
-
-    def step(self, action):
-        state, reward, done, info = self.env.step(action)
-
-        self.episode_step += 1
-        self.episode_reward += reward
-
-        if not self.rollout and done:
-            if self.episode_reward >= self.reward_max:
-                self.env.reset()
-                done = False
-
-            self.episode_step = 0
-            self.episode_reward = 0
-
-        return state, reward, done, info
-
-
 # Log scale rewards
+
+
 class ShapeReward(gym.Wrapper):
     def __init__(self, env, rollout):
         super(ShapeReward, self).__init__(env)
@@ -267,7 +243,6 @@ def create_env(config):
     env = ProcgenEnvWrapper(config)
     env = ReduceActions(env)
     env = DiffStack(env, 2)
-    # env = ContinuousLife(env, rollout, return_max)
     # env = FrameStack(env, 3)
     env = ShapeReward(env, rollout)
 
