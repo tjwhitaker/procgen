b9c6b6bec9ce1c92d2af99b3eec0260e36f26d47
Tim Whitaker <camp4climber@gmail.com>
2020-10-13 09:46:33 -0700

    Bigger final conv layer and remove two extra residual blocks

--- models/fixup.py
+++ models/fixup.py
@@ -35,23 +35,23 @@ class FixupCNN(TorchModelV2, nn.Module):
 
         layers = []
 
-        for depth_out in [32, 64, 64]:
+        for depth_out in [32, 64, 128]:
             layers.extend([
                 nn.Conv2d(depth_in, depth_out, 3, padding=1),
                 nn.MaxPool2d(3, stride=2, padding=1),
-                FixupResidual(depth_out, 8),
-                FixupResidual(depth_out, 8),
+                FixupResidual(depth_out, 6),
+                FixupResidual(depth_out, 6),
             ])
             depth_in = depth_out
 
-        layers.extend([
-            FixupResidual(depth_in, 8),
-            FixupResidual(depth_in, 8),
-        ])
+        # layers.extend([
+        #     FixupResidual(depth_in, 8),
+        #     FixupResidual(depth_in, 8),
+        # ])
 
         self.conv_layers = nn.Sequential(*layers)
 
-        self.hidden_fc = nn.Linear(in_features=4096, out_features=256)
+        self.hidden_fc = nn.Linear(in_features=8192, out_features=256)
         self.logits_fc = nn.Linear(in_features=256, out_features=num_outputs)
         self.value_fc = nn.Linear(in_features=256, out_features=1)
 
