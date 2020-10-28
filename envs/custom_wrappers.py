dd5dee6136a03ae37312eeaa25f789dfc2289c2b
Tim Whitaker <camp4climber@gmail.com>
2020-10-27 23:02:24 -0700

    Implement reward normalization

--- envs/custom_wrappers.py
+++ envs/custom_wrappers.py
@@ -80,14 +80,133 @@ class ShapeReward(gym.Wrapper):
 
         return state, reward, done, info
 
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+
+# Code from https://github.com/MadryLab/implementation-matters
+# https://arxiv.org/abs/2005.12729
+
+
+class RunningStat(object):
+    def __init__(self, shape):
+        self._n = 0
+        self._M = np.zeros(shape)
+        self._S = np.zeros(shape)
+
+    def push(self, x):
+        x = np.asarray(x)
+        assert x.shape == self._M.shape
+        self._n += 1
+        if self._n == 1:
+            self._M[...] = x
+        else:
+            oldM = self._M.copy()
+            self._M[...] = oldM + (x - oldM) / self._n
+            self._S[...] = self._S + (x - oldM) * (x - self._M)
+
+    @property
+    def n(self):
+        return self._n
+
+    @property
+    def mean(self):
+        return self._M
+
+    @property
+    def var(self):
+        return self._S / (self._n - 1) if self._n > 1 else np.square(self._M)
+
+    @property
+    def std(self):
+        return np.sqrt(self.var)
+
+    @property
+    def shape(self):
+        return self._M.shape
+
+
+class Identity:
+    '''
+    A convenience class which simply implements __call__
+    as the identity function
+    '''
+
+    def __call__(self, x, *args, **kwargs):
+        return x
+
+    def reset(self):
+        pass
+
+
+class RewardFilter:
+    """
+    "Incorrect" reward normalization [copied from OAI code]
+    Incorrect in the sense that we 
+    1. update return
+    2. divide reward by std(return) *without* subtracting and adding back mean
+    """
+
+    def __init__(self, prev_filter, shape, gamma):
+        assert shape is not None
+        self.gamma = gamma
+        self.prev_filter = prev_filter
+        self.rs = RunningStat(shape)
+        self.ret = np.zeros(shape)
+
+    def __call__(self, x, **kwargs):
+        x = self.prev_filter(x, **kwargs)
+        self.ret = self.ret * self.gamma + x
+        self.rs.push(self.ret)
+        x = x / (self.rs.std + 1e-8)
+        return x
+
+    def reset(self):
+        self.ret = np.zeros_like(self.ret)
+        self.prev_filter.reset()
+
+
+class NormalizeReward(gym.Wrapper):
+    def __init__(self, env, rollout):
+        super(NormalizeReward, self).__init__(env)
+        self.reward_filter = Identity()
+        self.reward_filter = RewardFilter(
+            self.reward_filter, shape=(), gamma=0.999)
+        self.total_true_reward = 0.0
+
+    def reset(self):
+        # Reset the state, and the running total reward
+        start_state = self.env.reset()
+        self.total_true_reward = 0.0
+        self.reward_filter.reset()
+
+        return start_state
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        self.total_true_reward += reward
+        _reward = self.reward_filter(reward)
+
+        return state, _reward, done, info
+
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+
 
 def create_env(config):
     config = copy(config)
+    rollout = config.pop("rollout")
 
     env = ProcgenEnvWrapper(config)
     env = DiffStack(env, 2)
     # env = FrameStack(env, 4)
     # env = ShapeReward(env, rollout)
+    env = NormalizeReward(env, rollout)
 
     return env
 
